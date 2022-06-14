f7dc397dacd7d9b5698285ca23d6e9c1c0053a65
vikram4026 <gmvikramgmv@gmail.com>
2022-06-14 20:22:48 +0530

    This Vinoth

--- src/com/testing/SampleClass.java
+++ src/com/testing/SampleClass.java
@@ -1,11 +1,15 @@
 package com.testing;
 
 public class SampleClass {
-	
+
+	private void vinoth() {
+		System.out.println("Vinoth Here....");
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello GitHub");
-		
+
 	}
 
 }
