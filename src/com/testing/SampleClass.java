251e94513ccff56fa27215a6fe1fa011081d0efe
vikram4026 <gmvikramgmv@gmail.com>
2022-06-14 20:03:12 +0530

    This Sam

--- src/com/testing/SampleClass.java
+++ src/com/testing/SampleClass.java
@@ -2,6 +2,11 @@ package com.testing;
 
 public class SampleClass {
 	
+	private void sam() {
+
+		System.out.println("This is Sam here");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub");
